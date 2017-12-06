e14f213934a084c59c4fbedd5cfd82e9c710dcd8
vkalia602 <vkalia602@gmail.com>
2017-12-06 01:38:15 +0000

    Fixed format to print double digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(00,100):
     if i < 99:
-        print("{:d}".format(i), end=", ")
+        print("{:02d}".format(i), end=", ")
     else:
-        print("{:d}".format(i))
+        print("{:02d}".format(i))
