3d1c547f590df84b21a006a5de37d544e3cf4ede
vkalia602 <267@holbertonschool.com>
2017-12-06 06:02:59 +0000

    Fixed print statements to print in the same line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 n = number % 10
-print("Last digit of {:d} is".format(number))
+print("Last digit of {:d} is".format(number), end=" ")
 if n > 5:
     print("{:d} and is greater than 5".format(n))
 elif n == 0:
