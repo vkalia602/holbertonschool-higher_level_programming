d940fb98437b79891b8f1e6f03dc3a8a894663f2
vkalia602 <vkalia602@gmail.com>
2017-12-06 01:35:30 +0000

    Fixed print format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print(i, end=", ")
+        print("{:d}".format(i), end=", ")
     else:
-        print(i)
+        print("{:d}".format(i))
