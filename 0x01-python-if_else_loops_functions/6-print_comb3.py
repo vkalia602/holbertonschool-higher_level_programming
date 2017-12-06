eb08fa9432f0082908d4accaa5201296e04aa1ae
vkalia602 <vkalia602@gmail.com>
2017-12-06 02:01:16 +0000

    First Commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            print("{:d}{:d}".format(i, j), end="")
+            if i == 8 and j == 9:
+                break
+            else:
+                print(", ", end="")
+print("")
