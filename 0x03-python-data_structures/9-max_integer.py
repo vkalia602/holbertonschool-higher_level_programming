4d6d1582c692fc153da6cb732c65adb3455dd545
vkalia602 <vkalia602@gmail.com>
2017-12-13 04:25:24 +0000

    First Commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    i = 0
+    x = my_list[0]
+    while (i < (len(my_list) - 1)):
+        if x < my_list[i]:
+            x = my_list[i]
+        i = i + 1
+    return x
