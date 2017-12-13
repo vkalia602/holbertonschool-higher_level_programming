b633d6dcfac6a21b8ce0bc29387af672806d5b82
vkalia602 <vkalia602@gmail.com>
2017-12-13 04:08:58 +0000

    Fixed edge case- empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None
     a = len(sentence)
-    b = sentence[0]
+    if len(sentence) == 0:
+        b = None
+    else:
+        b = sentence[0]
     return (a, b)
