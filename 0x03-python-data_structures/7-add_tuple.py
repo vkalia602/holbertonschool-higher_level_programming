578d56d66c26d5d474b22505cb0a87e38a5e684e
vkalia602 <vkalia602@gmail.com>
2017-12-13 03:52:44 +0000

    First Commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        a, b = 0, 0
+    elif len(tuple_a) == 1:
+        a, b = tuple_a[0], 0
+    else:
+        a, b = tuple_a[0], tuple_a[1]
+    if len(tuple_b) == 0:
+        c, d = 0, 0
+    elif len(tuple_b) == 1:
+        c, d = tuple_b[0], 0
+    else:
+        c, d = tuple_b[0], tuple_b[1]
+    return (a + c, b + d)
