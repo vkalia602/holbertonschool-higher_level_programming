a447e2ec9fa912e4ec9cc39975fa191f9c01bd34
vkalia602 <vkalia602@gmail.com>
2017-12-14 22:53:26 +0000

    Restructure function eliminating variable

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = matrix[:]
-        expo = lambda x: x ** 2
         for i in range(len(matrix)):
-            new_matrix[i] = list(map(expo, matrix[i]))
+            new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
         return new_matrix
