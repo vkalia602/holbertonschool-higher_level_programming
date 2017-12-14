806bc17383817a3d0e1695c3b2b05a9ec5d93ac1
vkalia602 <vkalia602@gmail.com>
2017-12-14 22:50:31 +0000

    Fixed spacing in lambda equation

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = matrix[:]
-        expo = lambda x: x**2
+        expo = lambda x: x ** 2
         for i in range(len(matrix)):
             new_matrix[i] = list(map(expo, matrix[i]))
         return new_matrix
