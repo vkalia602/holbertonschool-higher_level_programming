f76ecd75b38e22a29569932945394be2c5b73772
vkalia602 <vkalia602@gmail.com>
2017-12-14 22:40:23 +0000

    First Commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new_matrix = matrix[:]
+        expo = lambda x: x**2
+        for i in range(len(matrix)):
+            new_matrix[i] = list(map(expo, matrix[i]))
+        return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,5 @@
+# 0x02. Python - import & modules
+This is an introductory project in Python import and modules
+## File Descriptions
+*0-square_matrix_simple.py
+**Function that computes the square value of all integers of a matrix.
