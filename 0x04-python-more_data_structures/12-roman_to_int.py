e5ff51cf195a5777ea7bd6e63bf29144f24b8128
vkalia602 <vkalia602@gmail.com>
2017-12-15 06:09:42 +0000

    Fixed not a string check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     i = 0
     total = 0
+    if not roman_string or roman_string == None:
+        return 0
     for idx in range(1, len(roman_string)):
         if roman[roman_string[idx - 1]] < roman[roman_string[idx]]:
             i += 1
