04fc21c4987a8c0e15d7048ad053ec557cfcbd5c
vkalia602 <vkalia602@gmail.com>
2017-12-15 06:06:43 +0000

    First Commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i = 0
+    total = 0
+    for idx in range(1, len(roman_string)):
+        if roman[roman_string[idx - 1]] < roman[roman_string[idx]]:
+            i += 1
+    if i == 0:
+        for idx in range(len(roman_string)):
+            total += roman[roman_string[idx]]
+    elif i < len(roman_string):
+        for idx in range(i, len(roman_string)):
+            total += roman[roman_string[idx]]
+        for idx in range(0, i):
+            total -= roman[roman_string[idx]]
+    elif i == len(roman_string):
+        for idx in range(i):
+            total -= roman[roman_string[idx]]
+    return total
