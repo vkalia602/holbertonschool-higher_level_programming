6a17f3fc39444857875bf90bffeea262289f85ad
vkalia602 <vkalia602@gmail.com>
2017-12-16 00:55:34 +0000

    restructured code to include bigger values

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    i = 0
+    idx = len(roman_string) - 1
     total = 0
-    if not roman_string or roman_string == None:
+    if not roman_string or roman_string is None:
         return 0
-    for idx in range(1, len(roman_string)):
-        if roman[roman_string[idx - 1]] < roman[roman_string[idx]]:
-            i += 1
-    if i == 0:
-        for idx in range(len(roman_string)):
+
+    while (idx >= 0):
+        if idx > 0 and roman[roman_string[idx]] > roman[roman_string[idx - 1]]:
+            total += roman[roman_string[idx]] - roman[roman_string[idx - 1]]
+            idx -= 2
+        else:
             total += roman[roman_string[idx]]
-    elif i < len(roman_string):
-        for idx in range(i, len(roman_string)):
-            total += roman[roman_string[idx]]
-        for idx in range(0, i):
-            total -= roman[roman_string[idx]]
-    elif i == len(roman_string):
-        for idx in range(i):
-            total -= roman[roman_string[idx]]
+            idx -= 1
     return total
