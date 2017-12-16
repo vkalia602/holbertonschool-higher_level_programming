57207f401213ce22261fc6d7e4fafff9a2272fed
vkalia602 <vkalia602@gmail.com>
2017-12-16 01:16:37 +0000

    Fixed None check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    idx = len(roman_string) - 1
-    total = 0
-    if not roman_string or roman_string is None:
+    if roman_string is None or type(roman_string) is not str:
         return 0
-
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    idx = len(roman_string) - 1
     while (idx >= 0):
         if idx > 0 and roman[roman_string[idx]] > roman[roman_string[idx - 1]]:
             total += roman[roman_string[idx]] - roman[roman_string[idx - 1]]
