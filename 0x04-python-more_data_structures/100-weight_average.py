3b64341a6997e7bcfe60e6d638b344e3147c8976
vkalia602 <vkalia602@gmail.com>
2017-12-30 02:12:40 +0000

    First Commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    denom = 0
+    add = 0
+    for i in range(len(my_list)):
+        mult = 1
+        for j in range(len(my_list[i])):
+            mult *= my_list[i][j]
+            if j == 1:
+                denom += my_list[i][1]
+        add += mult
+    return add / denom
