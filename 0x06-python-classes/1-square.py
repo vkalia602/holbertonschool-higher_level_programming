5f155e6a59dde78b7b0c1fa24834c7618fc04a4b
vkalia602 <vkalia602@gmail.com>
2018-01-04 02:09:09 +0000

    Fixed doc string

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,8 +2,7 @@
 class Square:
     """Class Square initiated by size"""
     def __init__(self, size=None):
-        """Initialization of class Square with size
-        Args:
-            size: Size of the square
+        """Args:
+        size (int): Size of the square
         """
         self._square__size = size
