fa8eddbcbb0f8a0e7bee8eaa523ff2ec8b713449
vkalia602 <vkalia602@gmail.com>
2018-01-04 02:53:34 +0000

    First Commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square initiated by size"""
+    def __init__(self, size=0):
+        """Args:
+        size (int): Size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
