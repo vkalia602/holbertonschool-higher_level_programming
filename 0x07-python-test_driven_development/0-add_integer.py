db9577e4fa445131312866fbf405b00337ab8626
vkalia602 <267@holbertonschool.com>
2018-01-08 04:58:00 +0000

    Added doc string and comments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+Module to add integer
+
+
+"""
+
 
 def add_integer(a, b):
+    """Function adds 2 integers
 
+    """
+# check if a is not int or float
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
+# check if b is not an int or float
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+# adding and returning a and b
     return int(a) + int(b)
