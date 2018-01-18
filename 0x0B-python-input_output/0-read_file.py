7231e16fdabadf81063c7284248091989ad212d5
vkalia602 <vkalia602@gmail.com>
2018-01-18 04:38:14 +0000

    Fixed code to use read function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ function that reads a text file (UTF8) and prints it to stdout
 
 def read_file(filename=""):
     with open("my_file_0.txt", mode="r", encoding="utf-8") as my_file:
-        for line in my_file:
-            print(line, end="")
+            print(my_file.read(), end="")
