a56814b1b86ebecf784c2b637a573e97992113a1
vkalia602 <vkalia602@gmail.com>
2018-01-18 04:40:23 +0000

    Fixed doc string

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,11 @@ function that reads a text file (UTF8) and prints it to stdout
 
 
 def read_file(filename=""):
+    """
+    Function reads lines in a file
+
+    Args:
+        filename (str): File that needs to be read
+    """
     with open("my_file_0.txt", mode="r", encoding="utf-8") as my_file:
             print(my_file.read(), end="")
