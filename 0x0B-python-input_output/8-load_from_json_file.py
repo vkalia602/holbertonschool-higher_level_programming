838714fa0a01b32a3a65c2b74b1dfe8126ebe9f9
vkalia602 <vkalia602@gmail.com>
2018-01-18 04:50:36 +0000

    Fixed utf

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -9,5 +9,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, mode="r") as my_file:
+    with open(filename, mode="r", encoding="utf-8") as my_file:
         return json.load(my_file)
