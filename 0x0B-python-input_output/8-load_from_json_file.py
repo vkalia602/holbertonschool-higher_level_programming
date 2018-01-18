2fdee130e4042080afdfdbb7f8a51c8bc59525d8
vkalia602 <vkalia602@gmail.com>
2018-01-18 04:53:25 +0000

    Fixed doc string

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -9,5 +9,11 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”
+
+    Args:
+        filename: name of the JSON file
+    """
     with open(filename, mode="r", encoding="utf-8") as my_file:
         return json.load(my_file)
