6f669cac0975214adc26c0fe17cff5bd47bb4aec
vkalia602 <vkalia602@gmail.com>
2018-03-01 23:36:02 +0000

    First Commit

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows contained in the database hbtn_0d_tvshows 
-SELECT genre_id, tv_shows.title
+SELECT tv_shows.title, genre_id
 FROM tv_show_genres
 RIGHT OUTER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title, tv_show_genres.genre_id
