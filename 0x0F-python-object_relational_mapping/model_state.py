428074a38fba199190fa6fce462d909eea42c20f
vkalia602 <vkalia602@gmail.com>
2018-03-24 05:34:56 +0000

    First Commit

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class State that inherits from Base and links to the MySQL table states
+    Attributes-
+    id- id that represents a column
+    name- represents a column of a string
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
