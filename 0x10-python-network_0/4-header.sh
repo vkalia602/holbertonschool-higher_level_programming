dcc928d3e6d6f9675781b55f482e4d2d82da9e76
vkalia602 <vkalia602@gmail.com>
2018-04-04 04:20:58 +0000

    almost first commit

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -H "X-HolbertonSchool-User-Id : 98"
+curl -s "$1" -H "X-HolbertonSchool-User-Id:98"
