2306d59366be1d3f09184d7117c2c9c0dc140ab1
vkalia602 <vkalia602@gmail.com>
2018-04-04 01:36:33 +0000

    3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends request to it and displays the size
-curl -s "$1" | grep "Content-Length:" | cut -d " " -f2
+curl -Is "$1" | grep "Content-Length:" | cut -d " " -f2
