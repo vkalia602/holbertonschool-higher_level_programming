84ea004989ab7cab4ec394799cfa1c121f86e9d3
vkalia602 <vkalia602@gmail.com>
2018-04-04 04:45:59 +0000

    First Commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL, and displays response
-curl -s -X POST -d  "subject: I will always be here for PLD&email: hr@holbertonschool.com" "$1"
+curl -sX POST -d  "subject=I will always be here for PLD&email=hr@holbertonschool.com" "$1"
