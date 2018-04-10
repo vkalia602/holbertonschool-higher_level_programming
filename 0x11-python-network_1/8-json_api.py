2d50aa69a32de3fccb3b2108c7a75bce1711adad
vkalia602 <vkalia602@gmail.com>
2018-04-10 03:59:07 +0000

    First commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that takes a letter and sends a post request
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    try:
+        sys.argv[1]
+        q = sys.argv[1]
+    except IndexError:
+        q = ""
+
+
+    r = requests.post("http://0.0.0.0:5000/search_user", {'q': q})
+    data = {}
+    data = r.json()
+
+    if len(data) == 0:
+        print("No result")
+    elif data:
+        print("[{}] {}".format(data.get('id'), data.get('name')))
+    else:
+        print("Not a valid JSON")
