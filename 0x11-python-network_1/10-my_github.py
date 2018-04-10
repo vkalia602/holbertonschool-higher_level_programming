ca35694196cd62fb52fd92457d7ebfadb984f4da
vkalia602 <vkalia602@gmail.com>
2018-04-10 05:27:31 +0000

    First commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes your Github credentials (username and password) and uses
+the Github API to display your id
+"""
+if __name__ == "__main__":
+    from requests.auth import HTTPBasicAuth
+    import requests
+    import sys
+
+    un = sys.argv[1]
+    pswd = sys.argv[2]
+
+    r = requests.get("https://api.github.com/{}".format(un),
+                     auth=HTTPBasicAuth(un, pswd))
+    data = r.json()
+    print(data.get('id'))
