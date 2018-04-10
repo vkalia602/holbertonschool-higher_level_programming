9c5476ffc2ec39247f81d35475d00c42a7d3cf69
vkalia602 <vkalia602@gmail.com>
2018-04-10 18:04:36 +0000

    Fixed url for request.get

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     un = sys.argv[1]
     pswd = sys.argv[2]
 
-    r = requests.get("https://api.github.com/{}".format(un),
+    r = requests.get("https://api.github.com/user".format(un),
                      auth=HTTPBasicAuth(un, pswd))
     data = r.json()
+
     print(data.get('id'))
