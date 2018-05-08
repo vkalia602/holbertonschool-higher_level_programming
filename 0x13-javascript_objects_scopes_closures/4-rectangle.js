2d71fef7870b92bfa47f025e3798c0ea6d5cdcc4
vkalia602 <vkalia602@gmail.com>
2018-05-08 04:20:17 +0000

    First Commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let strn = '';
+      for (let j = 0; j < this.width; j++) {
+        strn = strn + 'X';
+      }
+      console.log(strn);
+    }
+  }
+  rotate () {
+    var hold = this.height;
+    this.height = this.width;
+    this.width = hold;
+  }
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
