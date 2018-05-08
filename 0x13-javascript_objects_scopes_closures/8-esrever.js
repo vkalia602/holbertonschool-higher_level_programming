c45269be3859c6c10be683fb5cff9bc7ee378d11
vkalia602 <vkalia602@gmail.com>
2018-05-08 05:29:42 +0000

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// function that returns the reversed version of a list
+exports.esrever = function (list) {
+  let rev = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    rev.push(list[i]);
+  }
+  return (rev);
+};
