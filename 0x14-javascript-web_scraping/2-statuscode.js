cf74a67ebb64b40edc2aae28d4b1c9a094683513
vkalia602 <vkalia602@gmail.com>
2018-05-09 01:09:22 +0000

    First commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 // Script that display the status code of a GET request
 
-let requestURL = process.argv[2]
+let requestURL = process.argv[2];
 let request = require('request');
 
-request(requestURL, function(err, response, body) {
-    if (err){
-	console.log(err)
-    }
-    else {
-	console.log('code: ' + response.statusCode)
-    }
-
+request(requestURL, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
