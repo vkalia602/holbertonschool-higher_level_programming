09f5bf95639292b2cdff0296bb951e60c2381d85
vkalia602 <vkalia602@gmail.com>
2018-05-09 00:26:22 +0000

    First commit

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that writes a string to a file
+
+let filename = process.argv[2];
+let fs = require('fs');
+
+fs.writeFile(filename, process.argv[3], 'utf8', (err) => {
+    if (err)
+	console.log(err);
+})
